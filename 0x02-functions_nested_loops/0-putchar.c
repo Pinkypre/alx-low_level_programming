d7d74fb85991baed6ce0ee1e45fd37c05ecf8d3b
Pinkypre <preciouspncube@gmail.com>
2023-02-21 16:03:25 -0800

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,16 @@
+/*
+ * File: 0-putcher.c
+ * Auth: Brennan D Baraban
+ *
+ */
+
 #include "main"
 
 /**
-  * main - Entry point
-  *
-  * Return: Always 0
-  */
+ * main - Prints "Holberton" followed by a new line.
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
 	_putchar('H');
