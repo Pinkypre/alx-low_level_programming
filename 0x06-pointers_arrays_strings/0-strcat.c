c9545f983c0e8311ef26891f657a58cebc62dce7
Pinkypre <preciouspncube@gmail.com>
2023-03-02 13:20:18 -0800

    2-strncpy

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -2,23 +2,20 @@
 #include <stdio.h>
 
 /**
- * _strcat - Concatenates two strings
- * @dest: The destination string
- * @src: The source string
- * Return: A pointer to the resulting string dest
+ * * _strcat - Cocatenates two strings
+ * @dest: pointer destination
+ * @src: pointer source
+ * Return: void
  */
 char *_strcat(char *dest, char *src)
 {
-	int dlen = 0, i;
-	while (dest[dlen])
-	{
-		dlen++;
+	int a = -1, i;
+	for (i =0; dest[i] != '\0'; i++);
+	do{
+	    a++;
+	    dest[i] = src[a];
+	    i++;
 	}
-	for (i = 0; src[i] != 0; i++)
-	{
-		dest[dlen] = src[i];
-		dlen++;
-	}
-	dest[dlen] = '\0';
+	while(src[a] != '\0');
 	return (dest);
 }	
