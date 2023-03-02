63f28689c9a9074fe5882768ed901c8190b297cb
Pinkypre <preciouspncube@gmail.com>
2023-03-02 14:32:39 -0800

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -17,13 +17,14 @@ char *cap_string(char *str)
 				str[index - 1] == '\t' ||
 				str[index - 1] == '\n' ||
 				str[index - 1] == '0' ||
+				str[index - 1] == ',' ||
 				str[index - 1] == ';' ||
 				str[index - 1] == '.' ||
 				str[index - 1] == '!' ||
 				str[index - 1] == '?' ||
 				str[index - 1] == '"' ||
-				str[index - 1] == '{' ||
-				str[index - 1] == '}' ||
+				str[index - 1] == '(' ||
+				str[index - 1] == ')' ||
 				str[index - 1] == '{' ||
 				str[index - 1] == '}' ||
 				index == 0)
