5635e0a884406baf59123aa3a00962c75bed1089
Pinkypre <preciouspncube@gmail.com>
2023-03-09 14:50:44 -0800

    2-strlen_recursion.c

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,17 +1,23 @@
 include "main.h"
 
 /**
- * _strlen_recursion - Calculate the length of a string.
- * @s: The string to be count
+ * factorial - Returns the factorial of a given number.
+ * @n: The number to find the factorial of.
  *
- * Return: integer value
+ * Return: If n > 0 - the factorial of n.
+ *         If n < 0 - 1 to indicate an error.
  */
-int _strlen_recursion(char *s)
+int factorial(int n)
 {
-if (*s == 0)
-{
-return (0);
-}
-s++;
-return (_strlen_recursion(s)  +  1);
+	int result = n;
+
+	if (n < 0)
+		return (-1);
+
+	else if (n >= 0 && n <= 1)
+		return (1);
+
+	result *= factorial(n - 1);
+
+	return (result);
 }
