4b5acabc1e45d78d353f6b8a53ed673d01d2daa0
Pinkypre <preciouspncube@gmail.com>
2023-03-12 13:25:20 -0700

    2-strlen_recursion.c

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,23 +1,16 @@
-include "main.h"
+#include "main.h"
 
 /**
- * factorial - Returns the factorial of a given number.
- * @n: The number to find the factorial of.
+ * factorial - returns the factorial of a number
+ * @n: number to return the factorial from
  *
- * Return: If n > 0 - the factorial of n.
- *         If n < 0 - 1 to indicate an error.
+ * Return: factorial of n
  */
 int factorial(int n)
 {
-	int result = n;
-
 	if (n < 0)
 		return (-1);
-
-	else if (n >= 0 && n <= 1)
+	if (n == 0)
 		return (1);
-
-	result *= factorial(n - 1);
-
-	return (result);
+	return (n * factorial(n - 1));
 }
