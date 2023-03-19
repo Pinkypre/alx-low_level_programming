aee161da6c77303e7ae2c8aa5499450c24f4de01
Pinkypre <preciouspncube@gmail.com>
2023-03-19 11:04:45 -0700

    0-memset.c

--- 0x07-pointers_arrays_strings/_putchar.c
+++ 0x07-pointers_arrays_strings/_putchar.c
@@ -8,8 +8,7 @@
  * on error,-1 is returned, and error is set appropriately
  *
  */
-
-  int _putchar(char c)
-  {	 
-	return (write(1,&c, 1));
-  }
+int _putchar(char c)
+{	 
+return (write(1,&c, 1));
+}
