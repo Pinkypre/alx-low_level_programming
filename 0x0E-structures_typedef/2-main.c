a208ea2616ea0c827fd95c997d999f31e1a24773
Pinkypre <preciouspncube@gmail.com>
2023-03-20 10:14:57 -0700

    2-print_dog.c

--- 0x0E-structures_typedef/2-main.c
+++ 0x0E-structures_typedef/2-main.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include "dog.h"
-
 /**
  * main - check the code
  *
@@ -8,11 +7,10 @@
  */
 int main(void)
 {
-    struct dog my_dog;
-
-    my_dog.name = "Poppy";
-    my_dog.age = 3.5;
-    my_dog.owner = "Bob";
-    print_dog(&my_dog);
-    return (0);
+struct dog my_dog;
+my_dog.name = "Poppy";
+my_dog.age = 3.5;
+my_dog.owner = "Bob";
+print_dog(&my_dog);
+return (0);
 }
