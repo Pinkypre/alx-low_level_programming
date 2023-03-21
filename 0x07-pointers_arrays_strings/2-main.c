a70cc6584542c64cfed90df5d16a695f7d1529bb
Pinkypre <preciouspncube@gmail.com>
2023-03-21 06:44:35 -0700

    1-memcpy.c

--- 0x07-pointers_arrays_strings/2-main.c
+++ 0x07-pointers_arrays_strings/2-main.c
@@ -1,3 +1,6 @@
+#include "main.h"
+#include <stdio.h>
+
 /**
  * main - check the code
  *
@@ -5,14 +8,12 @@
  */
 int main(void)
 {
-    char *s = "hello";
-    char *f;
-
-    f = _strchr(s, 'l');
-
-    if (f != NULL)
-    {
-        printf("%s\n", f);
-    }
-    return (0);
+char *s = "hello";
+char *f;
+f = _strchr(s, 'l');
+if (f != NULL)
+{
+printf("%s\n", f);
+}
+return (0);
 }
--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -5,14 +5,13 @@
  *@c: input
  *Return Always 0 (Success)
  */
-char*_strchr(char*s,char c)
+char *_strchr(char *s, char c)
 {
-	int i = 0;
-	
-	for (; s[i] &gt;= &#39;\0&#39;; i++)
-	{
-		if(s[i] == c)
-			return (&s[i]);
-	}
-	return (0);
+int i = 0;
+for (; s[i] & gt; = &#39; \0 & #39;; i++)
+{
+if (s[i] == c)
+return (&s[i]);
+}
+return (0);
 }
