7c259bad62d239cc4a88ec71def6ada821a230d7
Pinkypre <preciouspncube@gmail.com>
2023-03-21 07:00:33 -0700

    5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,25 +1,23 @@
-#include &quot;main.h&quot;
+#include "main.h"
 /**
-* _strstr - Entry point
-* @haystack: input
-* @needle: input
-* Return: Always 0 (Success)
-*/
+ * _strstr - Entry point
+ * @haystack: input
+ * @needle: input
+ * Return: Always 0 (Success)
+ */
 char *_strstr(char *haystack, char *needle)
 {
 for (; *haystack != '\0'; haystack++)
 {
 char *l = haystack;
 char *p = needle;
-while (*l == *p && *p != '/0')
+while (*l == *p && *p != '\0')
 {
 l++;
 p++;
 }
 if (*p == '\0')
 return (haystack);
-
 }
-
 return (0);
 }
