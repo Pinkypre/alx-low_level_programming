c77d4198d823c86244fbec9d5ba53e7eeb422789
Pinkypre <PreciousPncube@gmail.com>
2023-04-03 08:32:57 -0700

    1-listint_len.c

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+* listint_len - returns the number of elements in a linked lists
+* @h: linked list of type listint_t to traverse
+*
+* Return: number of nodes
+*/
+size_t listint_len(const listint_t *h)
+{
+size_t count = 0;
+while (h != NULL)
+{
+count++;
+h = h->next;
+}
+return (count);
+}
