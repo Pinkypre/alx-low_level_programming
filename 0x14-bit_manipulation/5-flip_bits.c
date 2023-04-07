9c1c9614154f382febb9aed7366f94be8a500e43
Pinkypre <PreciousPncube@gmail.com>
2023-04-07 12:52:31 -0700

    5-flip_bits.c

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * flip_bits - counts the number of bits to change
+ * to get from one number to another
+ * @n: The number
+ * @m: The number to flip into
+ *
+ * Return: number of bits to change
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+int i, count = 0;
+unsigned long int current;
+unsigned long int exclusive = n ^ m;
+for (i = 63; i >= 0; i--)
+{
+current = exclusive >> i;
+if (current & 1)
+count++;
+}
+return (count);
+}
