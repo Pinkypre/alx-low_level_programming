e0f9edfa8bbf21dfc12b61f16d2ceacde76e4680
Pinkypre <preciousPncube@gmail.com>
2023-05-19 08:17:48 -0700

    0-print_dlistint.c

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -1,38 +1,33 @@
-#ifndef LISTS_H
-#define LISTS_H
-
-#include <stdio.h>
+#ifndef LISTS
+#define LISTS
 #include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 
 /**
- * struct dlistint_s - singly linked list
+ * struct dlistint_s - doubly linked list
  * @n: integer
+ * @prev: points to the previous node
  * @next: points to the next node
  *
- * Description: singly linked list node structure
- * for alx project
+ * Description: doubly linked list node structure
+ * for Holberton project
  */
 typedef struct dlistint_s
 {
- int n;
- struct dlistint_s *next;
-} listint_t;
+    int n;
+    struct dlistint_s *prev;
+    struct dlistint_s *next;
+} dlistint_t;
 
 size_t print_dlistint(const dlistint_t *h);
 size_t dlistint_len(const dlistint_t *h);
-dlistint_t *add_nodeint(dlistint_t **head, const int n);
-dlistint_t *add_nodeint_end(dlistint_t **head, const int n);
+dlistint_t *add_dnodeint(dlistint_t **head, const int n);
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n);
 void free_dlistint(dlistint_t *head);
-void free_dlistint2(dlistint_t **head);
-int pop_dlistint(dlistint_t **head);
-dlistint_t *get_nodeint_at_index(dlistint_t *head, unsigned int index);
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index);
 int sum_dlistint(dlistint_t *head);
-dlistint_t *insert_nodeint_at_index(dlistint_t **head, unsigned int idx, int n);
-int delete_nodeint_at_index(dlistint_t **head, unsigned int index);
-dlistint_t *reverse_dlistint(dlistint_t **head);
-size_t print_dlistint_safe(const dlistint_t *head);
-size_t free_dlistint_safe(dlistint_t **h);
-dlistint_t *find_dlistint_loop(dlistint_t *head);
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n);
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
 
 #endif
-
